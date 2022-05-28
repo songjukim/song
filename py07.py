4e0718d3d954b738fdea4b46250069236d5ff9a1
songjukim <naheeya73@naver.com>
2022-05-28 12:21:25 +0900

    gg

---
+++ py07.py
@@ -0,0 +1,24 @@
+'''
+문제1> 3+3 = 6
+문제2> 5-2 = 3
+문제3> 5*2 = 10
+문제4> 10/2 = 5
+문제5> 10-3*3 = 1
+
+1번은 6
+2번은 3
+3번은 10
+4번은 5
+5번은 1
+'''
+
+number1 = input("문제1> 3+3 =")
+number2 = input("문제2> 5-2 =")
+number3 = input("문제3> 5*2 =")
+number4 = input("문제4> 10/2 =")
+number5 = input("문제5> 10-3*3 =")
+print("1번은 {}".format(number1))
+print("2번은 {}".format(number2))
+print("3번은 {}".format(number3))
+print("4번은 {}".format(number4))
+print("5번은 {}".format(number5))
