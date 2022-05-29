8eb668a527e047b424e469235f3fc246e9f6bee0
songjukim <naheeya73@naver.com>
2022-05-29 10:35:55 +0900

    11

---
+++ py12.py
@@ -0,0 +1,11 @@
+var1 = int(input("입력하세요1 :"))
+var2 = int(input("입력하세요2 :"))
+# input의 값은 문자열(글자)로 나온다
+print(var1-var2)
+
+var3 = float(input("파이값은 얼마일까요?"))
+print(var3)
+
+# int : 숫자 (소수점없는 숫자, 정수)
+# float : 숫자 (소수점 있는 숫자, 실수)
+# str : 글자 (문자열)
