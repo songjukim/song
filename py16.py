3f20a00684c73b420b01fcf6e800c9934eb7cefd
songjukim <naheeya73@naver.com>
2022-05-29 11:25:16 +0900

    연산자

--- py16.py
+++ py16.py
@@ -35,3 +35,10 @@ print("{} <= 10 : {}".format(a, a <= 10))
 print("{} == 10 : {}".format(a, a == 10))
 print("{} != 10 : {}".format(a, a != 10))
 
+a = 10
+b = 0
+print("{} > 0 and {} > 0 : {}".format(a, b, a > 0 and b > 0))
+print("{} > 0 or {} > 0 : {}".format(a, b, a > 0 or b > 0))
+print("not {} : {}".format(a, not a))
+print("not {} : {}".format(b, not b))
+
